7730e04520bfb936730cf0c926e269f4f7412087
timcs <timcs1274@gmail.com>
2022-07-08 14:16:36 -0400

    appended to the function to the main table variable

--- Assets/script.js
+++ Assets/script.js
@@ -1,14 +1,17 @@
+
+//Table function
 var createTable = (row, col) => {
     var tableBody = document.body;
     var tableMain = document.createElement("table");
-//add for loop to insert rows and columns
-for (i = 0; i < row; i++) {
-    var tblRow = tableMain.insertRow();
-    for (x = 0; x < col; x++) {
-        var tblCell = tblRow.insertCell();
-        tblCell.appendChild(document.createTextNode(""));
-        tblCell.style.border = "2px solid gray";
+//for loop to insert rows and columns
+    for (i = 0; i < row; i++) {
+        var tblRow = tableMain.insertRow();
+        for (x = 0; x < col; x++) {
+            var tblCell = tblRow.insertCell();
+            tblCell.appendChild(document.createTextNode(""));
+            tblCell.style.border = "2px solid gray";
+        }
     }
-}
+    tableBody.appendChild(tableMain);
 };
 createTable(2, 4);
