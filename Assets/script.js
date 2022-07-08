55b0f698aca9828483356713649d6720d7d3ae1e
Jondo <chet9880@gmail.com>
2022-07-08 14:08:34 -0400

    added all checkboxes to apiurl call, now I need to create the jquery ajax request function

--- Assets/script.js
+++ Assets/script.js
@@ -9,6 +9,19 @@ var highProtein = document.querySelector("#high-protein")
 var lowCarb = document.querySelector("#low-carb")
 var lowSodium = document.querySelector("#low-sodium")
 
+var vegan = document.querySelector("#vegan")
+var vegetarian = document.querySelector("#vegetarian")
+var paleo = document.querySelector("#paleo")
+
+var american = document.querySelector("#american")
+var asian = document.querySelector("#asian")
+var indian = document.querySelector("#indian")
+var mediterranean = document.querySelector("#mediterranean")
+
+var breakfast = document.querySelector("#breakfast")
+var lunch = document.querySelector("#lunch")
+var dinner = document.querySelector("#dinner")
+
 var searchBar = document.querySelector("#search-bar")
 
 // example apiEdamam request = https://api.edamam.com/api/recipes/v2?type=public&q=chicken&health=vegan&health=vegetarian&app_key=2658fb427c224a591a4dd3e5d4db9160&app_id=8426c0e6
@@ -22,9 +35,9 @@ filter types
 
 &health=[vegan, vegetarian, paleo] arraystring
 
-&cuisineType=[American, Asian, Indian, Mediterranean] arraystring
+&cuisineType=[american, asian, indian, mediterranean] arraystring
 
-&mealType=[Breakfast, Lunch, Dinner] arraystring
+&mealType=[breakfast, lunch, dinner] arraystring
 
 */
 
@@ -39,34 +52,101 @@ $.ajax({
 })
 
 function getRecipes(event){
+    //prevent default on form
     event.preventDefault()
+    // origin for api request
     var apiEdamam = "https://api.edamam.com/api/recipes/v2?type=public"
-
+    //adding the search bar value to the api request
     var searchResult = searchBar.value.trim()
     if (searchResult !== null){
         apiEdamam = apiEdamam + "&q=" + searchResult.toLowerCase()
         console.log(searchResult)
         console.log(apiEdamam + " This includes the search result")
     }
-
+//-------------------------------------------------------------------------
+// If statements for Diet
     if (highProtein.checked){
         var hpInfo = "&diet=high-protein"
         apiEdamam = apiEdamam + hpInfo
-        console.log(apiEdamam + " high-protein")
+        console.log(apiEdamam + " high-protein yes")
     }
 
     if (lowCarb.checked){
         var lcInfo = "&diet=low-carb"
         apiEdamam = apiEdamam + lcInfo
-        console.log(apiEdamam + " low-carb")
+        console.log(apiEdamam + " low-carb yes")
     }
 
     if (lowSodium.checked){
         var lsInfo = "&diet=low-sodium"
         apiEdamam = apiEdamam + lsInfo
-        console.log(apiEdamam + " low-sodium")
+        console.log(apiEdamam + " low-sodium yes")
+    }
+//-------------------------------------------------------------------------
+// If statements for Health
+    if (vegan.checked){
+        var veganInfo = "&health=vegan"
+        apiEdamam = apiEdamam + veganInfo
+        console.log(apiEdamam + " vegan yes")
     }
 
+    if (vegetarian.checked){
+        var vegetarianInfo = "&health=vegetarian"
+        apiEdamam = apiEdamam + vegetarianInfo
+        console.log(apiEdamam + " vegetarian yes")
+    }
+
+    if (paleo.checked){
+        var paleoInfo = "&health=paleo"
+        apiEdamam = apiEdamam + paleoInfo
+        console.log(apiEdamam + " paleo yes")
+    }
+//-------------------------------------------------------------------------
+// If statments for Cuisine Type
+    if (american.checked){
+        var americanInfo = "&cuisineType=american"
+        apiEdamam = apiEdamam + americanInfo
+        console.log(apiEdamam + " american yes")
+    }
+
+    if (asian.checked){
+        var asianInfo = "&cuisineType=asian"
+        apiEdamam = apiEdamam + asianInfo
+        console.log(apiEdamam + " asian yes")
+    }
+
+    if (indian.checked){
+        var indianInfo = "&cuisineType=indian"
+        apiEdamam = apiEdamam + indianInfo
+        console.log(apiEdamam + " indian yes")
+    }
+
+    if (mediterranean.checked){
+        var mediterraneanInfo = "&cuisineType=mediterranean"
+        apiEdamam = apiEdamam + mediterraneanInfo
+        console.log(apiEdamam + " medit yes")
+    }
+//-------------------------------------------------------------------------
+// If statement for Meal Type
+    if (breakfast){
+        var breakfastInfo = "&mealType=breakfast"
+        apiEdamam = apiEdamam + breakfastInfo
+        console.log(apiEdamam + " breakfast yes")
+    }
+
+    if (lunch.checked){
+        var lunchInfo = "&mealType=lunch"
+        apiEdamam = apiEdamam + lunchInfo
+        console.log(apiEdamam + " lunch yes")
+    }
+
+    if (dinner.checked){
+        var dinnerInfo = "&mealType=dinner"
+        apiEdamam = apiEdamam + dinnerInfo
+        console.log(apiEdamam + " dinner yes")
+    }
+
+    // adding the app_key and app_id to the end of the api request
     apiEdamam = apiEdamam + edamam_app_key + edemam_app_id
     console.log(apiEdamam + " This is the full request with all paramaters selected")
 }
