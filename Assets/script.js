7ffab7bee73ae6c67089bbea29448ce6a4e368d5
timcs <timcs1274@gmail.com>
2022-07-08 14:02:25 -0400

    created function for the table generation

---
+++ Assets/script.js
@@ -0,0 +1,6 @@
+var createTable = (row, col) => {
+    var tableBody = document.body;
+    var tableMain = document.createElement("table");
+//add for loop to insert rows and columns
+};
+createTable(2, 4);
