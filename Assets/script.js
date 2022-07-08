34f8432f3977add95184257236dd9895bbc8ca42
Jondo <chet9880@gmail.com>
2022-07-08 13:44:04 -0400

    added search bar and checked boxes to api call

--- Assets/script.js
+++ Assets/script.js
@@ -1,9 +1,15 @@
-var apiEdamam = "https://api.edamam.com/api/recipes/v2"
-var edamam_app_key = "2658fb427c224a591a4dd3e5d4db9160"
-var edemam_app_id = "8426c0e6"
+
+var edamam_app_key = "&app_key=2658fb427c224a591a4dd3e5d4db9160"
+var edemam_app_id = "&app_id=8426c0e6"
 
 var apiCocktailRandom = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
 var subBtn = document.querySelector("#subBtn")
+
+var highProtein = document.querySelector("#high-protein")
+var lowCarb = document.querySelector("#low-carb")
+var lowSodium = document.querySelector("#low-sodium")
+
+var searchBar = document.querySelector("#search-bar")
 
 // example apiEdamam request = https://api.edamam.com/api/recipes/v2?type=public&q=chicken&health=vegan&health=vegetarian&app_key=2658fb427c224a591a4dd3e5d4db9160&app_id=8426c0e6
 
@@ -34,7 +40,35 @@ $.ajax({
 
 function getRecipes(event){
     event.preventDefault()
-    console.log(exampleEda)
+    var apiEdamam = "https://api.edamam.com/api/recipes/v2?type=public"
+
+    var searchResult = searchBar.value.trim()
+    if (searchResult !== null){
+        apiEdamam = apiEdamam + "&q=" + searchResult.toLowerCase()
+        console.log(searchResult)
+        console.log(apiEdamam + " This includes the search result")
+    }
+
+    if (highProtein.checked){
+        var hpInfo = "&diet=high-protein"
+        apiEdamam = apiEdamam + hpInfo
+        console.log(apiEdamam + " high-protein")
+    }
+
+    if (lowCarb.checked){
+        var lcInfo = "&diet=low-carb"
+        apiEdamam = apiEdamam + lcInfo
+        console.log(apiEdamam + " low-carb")
+    }
+
+    if (lowSodium.checked){
+        var lsInfo = "&diet=low-sodium"
+        apiEdamam = apiEdamam + lsInfo
+        console.log(apiEdamam + " low-sodium")
+    }
+
+    apiEdamam = apiEdamam + edamam_app_key + edemam_app_id
+    console.log(apiEdamam + " This is the full request with all paramaters selected")
 }
 
 subBtn.addEventListener("click", getRecipes)
