74237fa0dedc36fad02ef058d48c7f8397f5d98d
timcs <timcs1274@gmail.com>
2022-07-08 14:08:49 -0400

    added for loop for inserting cells

--- Assets/script.js
+++ Assets/script.js
@@ -4,6 +4,9 @@ var createTable = (row, col) => {
 //add for loop to insert rows and columns
 for (i = 0; i < row; i++) {
     var tblRow = tableMain.insertRow();
+    for (x = 0; x < col; x++) {
+        var tblCell = tblRow.insertCell();
+    }
 }
 };
 createTable(2, 4);
