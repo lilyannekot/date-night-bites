33725aab20364efe81f1a960dabed0bd442d5ba6
timcs <timcs1274@gmail.com>
2022-07-08 22:10:09 -0400

    added variable for naming cells

--- Assets/script.js
+++ Assets/script.js
@@ -3,10 +3,12 @@
 var createTable = (row, col) => {
     var tableBody = document.body;
     var tableMain = document.createElement("table");
+
 //for loop to insert rows and columns
     for (i = 0; i < row; i++) {
         var tblRow = tableMain.insertRow();
         for (x = 0; x < col; x++) {
+            var rowName = "cell" + i + x;
             var tblCell = tblRow.insertCell();
             tblCell.appendChild(document.createTextNode(""));
             tblCell.style.border = "2px solid gray";
@@ -14,15 +16,17 @@ var createTable = (row, col) => {
     }
     tableBody.appendChild(tableMain);
 };
+
+//amount of (rows, columns)
 createTable(2, 4);
 
 
 //Learn more button
-var learnMore = document.createElement("button");
-    learnMore.innerHTML = "Learn More";
-    learnMore.type = "submit";
-    learnMore.name = "learnMoreBtn";
-    document.body.appendChild(learnMore);
+// var learnMore = document.createElement("button");
+//     learnMore.innerHTML = "Learn More";
+//     learnMore.type = "submit";
+//     learnMore.name = "learnMoreBtn";
+//     document.body.appendChild(learnMore);
     
 
 
