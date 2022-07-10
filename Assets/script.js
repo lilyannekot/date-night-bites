d067dc7a0e1bc08ed7ee73dd159a0f906dfee1da
Jondo <chet9880@gmail.com>
2022-07-10 08:43:56 -0400

    added api cocktail call

--- Assets/script.js
+++ Assets/script.js
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
-// Save favorites to local storage
-=======
 // edamam app key and id key
 var edamam_app_key = "&app_key=2658fb427c224a591a4dd3e5d4db9160"
 var edemam_app_id = "&app_id=8426c0e6"
 //-------------------------------------------------------------------------
-// thecocktaildb random cocktail request apicall
-var apiCocktailRandom = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+
 //-------------------------------------------------------------------------
 //submit button
 var subBtn = document.querySelector("#subBtn")
@@ -69,6 +65,8 @@ filter types
 
 
 function getRecipes(event){
+    // thecocktaildb random cocktail request apicall
+    var apiCocktailRandom = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
     //prevent default on form
     event.preventDefault()
     // origin for api request
@@ -167,9 +165,20 @@ function getRecipes(event){
     apiEdamam = apiEdamam + edamam_app_key + edemam_app_id
     console.log(apiEdamam + " This is the full request with all paramaters selected")
 
+    apiCocktailCall(apiCocktailRandom);
     apiEdamamCall(apiEdamam);
+    
 }
+/*
 
+
+
+
+Need to add <img tags> inside table for validator when table is added
+
+
+
+*/
 function apiEdamamCall(apiEdamam){
 
     $.ajax({
@@ -226,13 +235,13 @@ function apiEdamamCall(apiEdamam){
         console.log("4th label",fourthLabel)
         console.log("4th img",fourthImage)
 
-        placeTableData(firstLabel, firstImg, secondLabel, secondImg, thirdLabel, thirdImage, fourthLabel, fourthImage);
+       // placeTableData(firstLabel, firstImg, secondLabel, secondImg, thirdLabel, thirdImage, fourthLabel, fourthImage);
     })
 
     
     
 }
-
+/*
 function placeTableData(firstLabel, firstImg, secondLabel, secondImg, thirdLabel, thirdImage, fourthLabel, fourthImage){
 
     head1.textContent = firstLabel
@@ -248,7 +257,109 @@ function placeTableData(firstLabel, firstImg, secondLabel, secondImg, thirdLabel
     img4.src = fourthImage
     
 }
+*/
+function apiCocktailCall(apiCocktailRandom){
+    $.ajax({
+        url: apiCocktailRandom,
+        method: 'GET'
+    }).then(function (response){
 
+    var drinkName = response.drinks[0].strDrink
+    console.log("Drink Name", drinkName)
+    var drinkCategory = response.drinks[0].strCategory
+    console.log("Drink Category", drinkCategory)
+    var drinkInstructions = response.drinks[0].strInstructions
+    console.log(drinkInstructions)
+    var drinkIngredients = []
+    var drinkMixAmount = []
+    var drinkIMA = []
+    /*
+    if (response.drinks[0].strIngredient1 !== null && response.drinks[0].strIngredient1 !== undefined){
+        drinkIngredients[0] = response.drinks[0].strIngredient1
+        drinkMixAmount[0] = response.drinks[0].strMeasure1
+    }
+
+    if (response.drinks[0].strIngredient2 !== null && response.drinks[0].strIngredient2 !== undefined){
+        drinkIngredients[1] = response.drinks[0].strIngredient2
+        drinkMixAmount[1] = response.drinks[0].strMeasure2
+    }
+
+    if (response.drinks[0].strIngredient3 !== null && response.drinks[0].strIngredient3 !== undefined){
+        drinkIngredients[2] = response.drinks[0].strIngredient3
+        drinkMixAmount[2] = response.drinks[0].strMeasure3
+    }
+
+    if (response.drinks[0].strIngredient4 !== null && response.drinks[0].strIngredient4 !== undefined){
+        drinkIngredients[3] = response.drinks[0].strIngredient4
+        drinkMixAmount[3] = response.drinks[0].strMeasure4
+    }
+
+    if (response.drinks[0].strIngredient5 !== null && response.drinks[0].strIngredient5 !== undefined){
+        drinkIngredients[4] = response.drinks[0].strIngredient5
+        drinkMixAmount[4] = response.drinks[0].strMeasure5
+    }
+
+    if (response.drinks[0].strIngredient6 !== null && response.drinks[0].strIngredient6 !== undefined){
+        drinkIngredients[5] = response.drinks[0].strIngredient6
+        drinkMixAmount[5] = response.drinks[0].strMeasure6
+    }
+
+    if (response.drinks[0].strIngredient7 !== null && response.drinks[0].strIngredient7 !== undefined){
+        drinkIngredients[6] = response.drinks[0].strIngredient7
+        drinkMixAmount[6] = response.drinks[0].strMeasure7
+    }
+
+    if (response.drinks[0].strIngredient8 !== null && response.drinks[0].strIngredient8 !== undefined){
+        drinkIngredients[7] = response.drinks[0].strIngredient8
+        drinkMixAmount[7] = response.drinks[0].strMeasure8
+    }
+
+    if (response.drinks[0].strIngredient9 !== null && response.drinks[0].strIngredient9 !== undefined){
+        drinkIngredients[8] = response.drinks[0].strIngredient9
+        drinkMixAmount[8] = response.drinks[0].strMeasure9
+    }
+
+    if (response.drinks[0].strIngredient10 !== null && response.drinks[0].strIngredient10 !== undefined){
+        drinkIngredients[9] = response.drinks[0].strIngredient10
+        drinkMixAmount[9] = response.drinks[0].strMeasure10
+    }
+
+    if (response.drinks[0].strIngredient11 !== null && response.drinks[0].strIngredient11 !== undefined){
+        drinkIngredients[10] = response.drinks[0].strIngredient11
+        drinkMixAmount[10] = response.drinks[0].strMeasure11
+    }
+
+    if (response.drinks[0].strIngredient12 !== null && response.drinks[0].strIngredient12 !== undefined){
+        drinkIngredients[11] = response.drinks[0].strIngredient12
+        drinkMixAmount[11] = response.drinks[0].strMeasure12
+    }
+
+    if (response.drinks[0].strIngredient13 !== null && response.drinks[0].strIngredient13 !== undefined){
+        drinkIngredients[12] = response.drinks[0].strIngredient13
+        drinkMixAmount[12] = response.drinks[0].strMeasure13
+    }
+
+    if (response.drinks[0].strIngredient14 !== null && response.drinks[0].strIngredient14 !== undefined){
+        drinkIngredients[13] = response.drinks[0].strIngredient14
+        drinkMixAmount[13] = response.drinks[0].strMeasure14
+    }
+
+    if (response.drinks[0].strIngredient15 !== null && response.drinks[0].strIngredient15 !== undefined){
+        drinkIngredients[14] = response.drinks[0].strIngredient15
+        drinkMixAmount[14] = response.drinks[0].strMeasure15
+    }
+
+    for (var i = 0; i < drinkIngredients.length; i++){
+        drinkIMA[i] = drinkMixAmount[i] + " " + drinkIngredients[i]
+        if (drinkIMA[i] === " "){
+            delete drinkIMA[i]
+            drinkIMA[i] = 1
+        }
+    }
+*/
+    console.log(drinkIMA)
+    console.log()
+    })
+}
 
 subBtn.addEventListener("click", getRecipes)
->>>>>>> c5b9023fcaa7c6e50b4b117ffba1ef35ca4b4f07
